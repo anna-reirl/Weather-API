de3b7649fc167b8963e57f651eb5052980b8da91
Anna <anna_agabekyan_anna@mail.ru>
2020-08-06 17:50:56 +0400

    feat: Displaying weather information in a component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,7 +5,15 @@ class Weather extends React.Component {
     render() {
         return (
             <div>
-               Weather
+                { this.props.city &&
+                <div>
+                <p>Местоположение: {this.props.city}, {this.props.country}</p>
+                <p>Температура: {this.props.temp}</p>
+                <p>Давление: {this.props.pressure}</p>
+                <p>Заход солнца: {this.props.sunset}</p>
+                </div>
+                }
+               
             </div>
         );
     }
